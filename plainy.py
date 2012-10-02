1949add7f4d373231e97b86651190c070af1ead3
Harald-Alfred Wurpts <h.wurpts@hr4you.de>
2012-10-02 11:50:19 +0200

    Hier ist ein Sammelpatch für Plainy
    1) Das Programm wurde an einigen Stellen umgeschrieben
       damit pylint keine Fehler mehr wirft.
    
    2) Der Import des Moduls docx wird nun sauber mit einer
       exception gefangen

--- plainy.py
+++ plainy.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Plainy. Einfach nur Plainy"""
 try:
     import sys
     import subprocess
@@ -19,48 +20,51 @@ def usage():
 
 if __name__ == '__main__':
     try:
-        dfile = sys.argv[1]
-    except:
+        DFILE = sys.argv[1]
+    except ImportError:
         usage()
 
-    print dfile
+    print DFILE
 
-    m = magic.open(magic.MAGIC_MIME)
-    m.load()
-    mime = m.file(dfile)
-    print mime
+    M = magic.open(magic.MAGIC_MIME)
+    M.load()
+    MIME = M.file(DFILE)
+    print MIME
 
-    if mime.startswith('application/pdf'):
-        print subprocess.Popen("pdftotext -layout " + dfile + " - ", \
+    if MIME.startswith('application/pdf'):
+        print subprocess.Popen("pdftotext -layout " + DFILE + " - ", \
             shell=True, stdout=subprocess.PIPE).communicate()[0]
 
-    if mime.startswith('application/vnd.oasis.opendocument.text'):
-        print subprocess.Popen("unzip -p '"+ dfile + \
+    if MIME.startswith('application/vnd.oasis.opendocument.text'):
+        print subprocess.Popen("unzip -p '"+ DFILE + \
         "' content.xml | xmlstarlet sel -N "\
         + "text='urn:oasis:names:tc:opendocument:xmlns:text:1.0'"\
         +"  -T -t -m '//text:p' -v . -n", shell=True, \
             stdout=subprocess.PIPE).communicate()[0]
 
-    if mime.startswith('text/rtf'):
-        print subprocess.Popen("unrtf --nopict -t text " + dfile \
+    if MIME.startswith('text/rtf'):
+        print subprocess.Popen("unrtf --nopict -t text " + DFILE \
             + " 2> /dev/null | sed -e '/^###.*/d' -e '/^----.*/d'"\
             , shell=True, \
             stdout=subprocess.PIPE).communicate()[0]
 
-    if mime.startswith('text/html'):
-        print subprocess.Popen("w3m -dump "+ dfile \
+    if MIME.startswith('text/html'):
+        print subprocess.Popen("w3m -dump "+ DFILE \
             , shell=True
             , stdout=subprocess.PIPE).communicate()[0]
 
 
-    if mime.startswith('application/zip') and dfile.endswith('.docx'):
-        import docx
-        document = docx.opendocx(dfile)
-        paratextlist = docx.getdocumenttext(document)
-        print '\n\n'.join(paratextlist)
+    if MIME.startswith('application/zip') and DFILE.endswith('.docx'):
+        try:
+            import docx
+        except ImportError:
+            print "Module fuer Docx nicht gefunden!"
+        DOCUMENT = docx.opendocx(DFILE)
+        PARATEXTLIST = docx.getdocumenttext(DOCUMENT)
+        print '\n\n'.join(PARATEXTLIST)
 
-    if mime.startswith('application/x-rar') and dfile.endswith('.rar'):
-        print subprocess.Popen("rar x " + dfile \
+    if MIME.startswith('application/x-rar') and DFILE.endswith('.rar'):
+        print subprocess.Popen("rar x " + DFILE \
             , shell=True, \
             stdout=subprocess.PIPE).communicate()[0]
 
